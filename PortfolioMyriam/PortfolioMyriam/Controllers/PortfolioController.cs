63d3d4ed33612169374daa1f9495106dd9ff9a28
Vincent Meijer <meijer.vincent@gmail.com>
2018-04-01 16:02:40 +0200

    Add initial Portfolio controller and views

---
+++ PortfolioMyriam/PortfolioMyriam/Controllers/PortfolioController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PortfolioMyriam.Data;
+using PortfolioMyriam.Models;
+
+namespace PortfolioMyriam.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PortfolioController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Portfolio
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.PortfolioItem.ToListAsync());
+        }
+
+        // GET: Portfolio/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var portfolioItem = await _context.PortfolioItem
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (portfolioItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(portfolioItem);
+        }
+
+        // GET: Portfolio/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Portfolio/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,ExternalReference")] PortfolioItem portfolioItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(portfolioItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(portfolioItem);
+        }
+
+        // GET: Portfolio/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var portfolioItem = await _context.PortfolioItem.SingleOrDefaultAsync(m => m.Id == id);
+            if (portfolioItem == null)
+            {
+                return NotFound();
+            }
+            return View(portfolioItem);
+        }
+
+        // POST: Portfolio/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,ExternalReference")] PortfolioItem portfolioItem)
+        {
+            if (id != portfolioItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(portfolioItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PortfolioItemExists(portfolioItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(portfolioItem);
+        }
+
+        // GET: Portfolio/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var portfolioItem = await _context.PortfolioItem
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (portfolioItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(portfolioItem);
+        }
+
+        // POST: Portfolio/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var portfolioItem = await _context.PortfolioItem.SingleOrDefaultAsync(m => m.Id == id);
+            _context.PortfolioItem.Remove(portfolioItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PortfolioItemExists(int id)
+        {
+            return _context.PortfolioItem.Any(e => e.Id == id);
+        }
+    }
+}
