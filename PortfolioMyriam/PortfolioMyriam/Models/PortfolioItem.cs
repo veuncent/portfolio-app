1699c8fb03ab3df2037b944586470eb159c050b3
Vincent Meijer <meijer.vincent@gmail.com>
2018-04-01 15:57:08 +0200

    Add initial Portfolio model and migrations.

---
+++ PortfolioMyriam/PortfolioMyriam/Models/PortfolioItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioMyriam.Models
+{
+    public class PortfolioItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ExternalReference { get; set; }
+    }
+}
