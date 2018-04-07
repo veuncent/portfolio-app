eea19131553ab9eaefe1391d96626308e5b7e526
Vincent Meijer <meijer.vincent@gmail.com>
2018-04-07 17:27:03 +0200

    Remove unnecessary usings

--- PortfolioMyriam/PortfolioMyriam/Models/PortfolioViewModels/ExternalReferenceViewModel.cs
+++ PortfolioMyriam/PortfolioMyriam/Models/PortfolioViewModels/ExternalReferenceViewModel.cs
@@ -1,9 +1,4 @@
 ﻿using PortfolioMyriam.Models.Enums;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PortfolioMyriam.Models
 {
